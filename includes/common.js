847ee39e4ab7b9b28d43aff0c4a0c8651a004ac3
Christian Yarros <yarros@google.com>
2023-01-25 14:29:29 +0000

    updates

--- includes/common.js
+++ includes/common.js
@@ -1,9 +1,9 @@
-function dedupe(group_by_cols) {
+function dedupe(table, group_by_cols) {
     return `
             SELECT k.*
             FROM (
                 SELECT ARRAY_AGG(row LIMIT 1)[OFFSET(0)] k 
-                FROM t1 row
+                FROM ${table} row
                 GROUP BY ${group_by_cols}
             )
             `
